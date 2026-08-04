ff49e3e78dab7fbd3cef6ab73224a0548b2bb417
BegSaad <mohdsaadstp2019@gmail.com>
2026-08-04 22:07:15 +0530

    feat:vctor icons android

--- src/screens/Maps/Maps.tsx
+++ src/screens/Maps/Maps.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { Ionicons } from '@react-native-vector-icons/ionicons';
 const Maps = () => {
   return (
     <View>
       <Text>Maps</Text>
+      <Ionicons
+  name="home"
+  size={30}
+  color="blue"
+/>
+<Ionicons name="camera-reverse" color="#000" size={24} />
     </View>
   )
 }
